cd9d93b135a42297e8901219953213bf9a657c31
LemanSachs29 <juanblancomoyano@gmail.com>
2024-10-10 19:02:48 +0200

    Fin de la tarde del 10/10

---
+++ ej9_tabMultiplicar.js
@@ -0,0 +1,26 @@
+let numero;
+
+function mutiplicationTable(num, lim = 10){
+
+
+    num = parseInt(num);
+    
+    if(num == NaN){
+        throw new TypeError("Valor del parámetro no válido.");
+    }else if(lim == NaN || lim <= 0 ){
+        lim = 10;
+    }
+
+    for(let i = 1; i<=lim; i++){
+        console.log(num + " X " + i + " = " + num*i);
+    }
+}
+
+try{
+numero = prompt("Número");
+lim = prompt("Límite (opcional)")
+mutiplicationTable(numero, lim);
+
+}catch(e){
+    console.log(e.message);
+}
